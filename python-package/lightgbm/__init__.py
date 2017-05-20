14797dbdfc2f951e7138835a86181c51a15338f4
Miguel González-Fierro <miguelgfierro@users.noreply.github.com>
2017-05-20 09:54:00 +0100

    update to v2 in init file

--- python-package/lightgbm/__init__.py
+++ python-package/lightgbm/__init__.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+2# coding: utf-8
 """LightGBM, Light Gradient Boosting Machine.
 
 Contributors: https://github.com/Microsoft/LightGBM/graphs/contributors
@@ -21,7 +21,7 @@ except ImportError:
     pass
 
 
-__version__ = 0.1
+__version__ = 0.2
 
 __all__ = ['Dataset', 'Booster',
            'train', 'cv',
